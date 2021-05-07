f4808d2a071f5fa861a9c2fd780396d4d72ba2c6
Paul Dechov <dechovsky@gmail.com>
2021-05-07 11:06:43 -0400

    Add partner ID to API request header

--- includes/class-wc-stripe-api.php
+++ includes/class-wc-stripe-api.php
@@ -54,9 +54,10 @@ class WC_Stripe_API {
 	 */
 	public static function get_user_agent() {
 		$app_info = array(
-			'name'    => 'WooCommerce Stripe Gateway',
-			'version' => WC_STRIPE_VERSION,
-			'url'     => 'https://woocommerce.com/products/stripe/',
+			'name'       => 'WooCommerce Stripe Gateway',
+			'version'    => WC_STRIPE_VERSION,
+			'url'        => 'https://woocommerce.com/products/stripe/',
+			'partner_id' => 'pp_partner_EYuSt9peR0WTMg',
 		);
 
 		return array(
